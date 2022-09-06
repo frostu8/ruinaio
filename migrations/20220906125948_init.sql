e7919d6888da8861824b09e7388a3647abf97280
frostu8 <frostu8@protonmail.com>
2022-09-06 16:38:19 -0400

    database init

---
+++ migrations/20220906125948_init.sql
@@ -0,0 +1,27 @@
+-- Nodes table
+CREATE TABLE node (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(256) NOT NULL,
+    content TEXT NOT NULL
+);
+
+-- Relationships
+CREATE TABLE relation (
+    parent_id INTEGER NOT NULL REFERENCES node(id) ON DELETE CASCADE,
+    child_id INTEGER NOT NULL REFERENCES node(id) ON DELETE CASCADE,
+
+    PRIMARY KEY (parent_id, child_id)
+);
+
+-- Images
+CREATE TABLE images (
+    -- Used to look up the file in the object storage.
+    hash CHAR(64) NOT NULL,
+    -- The actual filename.
+    filename VARCHAR(256) NOT NULL,
+    -- The parent node's id.
+    node_id INTEGER NOT NULL REFERENCES node(id) ON DELETE CASCADE,
+
+    PRIMARY KEY (filename, node_id)
+);
+
